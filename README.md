8a533f8021bbc8c9fc2d9381bedd4a375176dd49
LokeshSavlani224 <190320107554.ce.lokesh.savlani@gmail.com>
2023-01-24 18:30:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Lokesh-Repo-test" 
---
+++ introduction.js
@@ -0,0 +1,7 @@
+//Es1 Es2 Es3 1997-1999
+//ES5 2009
+//ES6 2015
+// yearly additions 2016 2017 2018
+
+document.getElementById("demo").innerHTML="abcdef";// does the work of getting the value of the element having id demo
+document.title="abcfgh";
